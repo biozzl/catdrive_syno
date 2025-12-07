cfdc7a65c5d72d5f4b3859760a4be8dc10539186
AllenZ <biozzl@gmail.com>
2025-12-07 17:58:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ exit 1
 function install() {
     cd /tmp/
     send_msg "downloading full.bin"
-    wget -N --no-check-certificate -O full.bin https://raw.githubusercontent.com/roadwide/catdrive-syno/master/full.bin
+    wget -N --no-check-certificate -O full.bin https://github.com/biozzl/catdrive_syno/raw/refs/heads/master/full.bin
     on_error_exit "download full.bin failed"
     
     send_msg "start writing mdt0"
